0670485580da3821fb294cf6b467f1f805b36563
thanghq12 <thanghq12@fpt.edu.vn>
2024-05-28 09:53:59 +0700

    update

--- src/main/java/com/example/sd18301/controller/ProductController.java
+++ src/main/java/com/example/sd18301/controller/ProductController.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -30,4 +33,39 @@ public class ProductController {
         model.addAttribute("products", products);
         return "product/index";
     }
+    // route trỏ đến view add
+    @GetMapping("/add")
+    public String addProduct() {
+        // return ra view add
+        return "";
+    }
+    // route store thêm 
+    @PostMapping("/store") 
+    public String store(Product pr) {
+        productReponsitory.save(pr);
+        return "redirect:/product/index"; // return lại về trang chủ 
+    }
+    //tạo trang chi tiết sản phẩm để sửa
+    @GetMapping("/edit/{id}")
+    public String editProduct(@PathVariable("id") Long id,Model model) {
+        Product product = productReponsitory.findById(id).orElseThrow(); 
+        model.addAttribute("product", product);
+        return ""; // return ra view edit
+    }
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") Long id,@ModelAttribute Product pr) {
+        Product existProduct = productReponsitory.findById(id).orElseThrow();
+        existProduct.setName(pr.getName());
+        existProduct.setPrice(pr.getPrice());
+        productReponsitory.save(existProduct); // cập nhập lại 
+        return "redirect:/product/index"; // return lại về trang chủ 
+    }
+    // xóa sản phẩm
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+         Product existProduct = productReponsitory.findById(id).orElseThrow();
+         productReponsitory.delete(existProduct);
+         return "redirect:/product/index"; 
+    }
+    
 }
